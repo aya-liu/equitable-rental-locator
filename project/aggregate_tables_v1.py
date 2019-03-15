5f9d1a555dabfe72bf9037224ba7c7d4a5830f0a
Vedika Ahuja <vedikaa@cs.uchicago.edu>
2019-03-15 11:22:29 -0500

    small adjustments

--- project/aggregate_tables_v1.py
+++ project/aggregate_tables_v1.py
@@ -19,10 +19,27 @@ def aggregate_table(locator_database, column_agg_by):
                        'stop_wi_quart_mi' : 'sum', 
                        'stop_wi_half_mi' : 'sum', 
                         })
-    
-    # agg = grouped.agg({'Addresss' : 'count', 
+
+    # agg = grouped.agg({'Address' : 'count', 
     #                    'stop_wi_quart_mi' : 'sum', 
     #                    'stop_wi_half_mi' : 'sum', 
-    #                     })
+    #                    'population' : 'sum',
+    #                    'evictions' : 'sum'
+    #                    'eviction-filings' : 'sum'
+    #                     'renter-occupied-households' : 'sum'
+    #                 })
+
     # 'population', 'eviction-filings', 'evictions'
+    #calculate rates
+    agg = agg.rename(columns={'Address': "num_cha_properties"})
+
+    agg['percent_wi_quart_mi_l_stop'] = agg['stop_wi_quart_mi']/agg['num_cha_properties']
+    agg['percent_wi_half_mi_l_stop'] = agg['stop_wi_half_mi']/agg['num_cha_properties']
+
+    # #calculate eviction rates
+    # agg['eviction_rate'] = agg['evictions']/agg['renter-occupied-households']
+    # agg['eviction_filing_rate'] = agg['eviction-filings']/agg['renter-occupied-households']
+
     return agg
+
+
--- project/build_database.py
+++ project/build_database.py
@@ -97,7 +97,8 @@ def merge_with_evict(cha, evict):
     evict_to_merge = evict[evict.year == '2016']
     evict_to_merge = evict_to_merge[["GEOID", "eviction-rate", 
                     "eviction-filing-rate", "population", "renter-occupied-households",
-                    "eviction-filings", "evictions", "renter-occupied-households"]]
+                    "eviction-filings", "evictions", "renter-occupied-households",
+                    "poverty-rate"]]
     merged_with_ev = pd.merge(cha.reset_index(), evict_to_merge, on="GEOID", 
                             how="left")
     return merged_with_ev
