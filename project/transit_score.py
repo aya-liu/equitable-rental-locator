c98ae279d8b1bb3849f7d04e1b58b0a56412fdc5
Vedika Ahuja <vedikaa@cs.uchicago.edu>
2019-03-14 20:25:01 -0500

    done!

--- project/transit_score.py
+++ project/transit_score.py
@@ -11,31 +11,6 @@ This program takes a gps coordinate and applies a score to
 that location based on how many bus stops and L stations are 
 within a certain vicinity of the locaton.
 '''
-def read_clean_landlords(problem_landlords_filepath):
-    '''
-    Read and clean the problem lands lords csv from the 
-    CHA site.
-
-    Downloaded from: 
-    https://www.chicago.gov/city/en/depts/bldgs/supp_info/building-code-scofflaw-list.html
-    '''
-    landlords = pd.read_csv(problem_landlords_filepath)
-    landlords = landlords.rename(columns={"LONGITUDE": "Long", "LATITUDE": "Lat", "ADDRESS": "Address"})
-
-    return landlords
-
-def landlords_apt_fuzzy_match(apt_df, landlords_df):
-    '''
-    Do a fuzzy match by gps coordinates on the dataset with problem
-    landlords and apartments. Match landlords and apartments that are
-    within .1 miles of each other. Export a csv with these matches,
-    and then manually check if the addresses are the same.
-    '''
-    cross_df = create_cross_join(apt_df, landlords_df, additional_columns=["Address"])
-
-    return cross_df
-
-
 def load_and_clean_cha(CHA_filename):
     '''
     Load and clean CHA data.
@@ -58,22 +33,9 @@ def load_and_clean_cha(CHA_filename):
     # correct an one-off location error
     cha.loc["4545145", "Long"] = -87.66593 
     cha.loc["4545145", "Lat"] = 41.772175
+    
     return cha
 
-
-def transit_score(orig_lat, orig_lon, apartments_dataset, L_stations, bus_stations):
-    '''
-    calculates manhattan distance between the location and 
-    the l stations and bustations and calculates a transit score based 
-    on the number of nearby stations
-
-    Score is based on the percentile ranking of close transit stations
-    in the city of Chicago
-
-    ex (99) means the location is closer to more stops than 99% of other
-    places in the dataset. (reconsider how to do this - might want to create a raw score)
-    '''
-    pass
 
 def clean_L_stations(L_stations_csv):
     '''
@@ -89,7 +51,8 @@ def clean_L_stations(L_stations_csv):
     L_stations["Lat"] = new[0]
     L_stations["Long"] = new[1]
     L_stations.drop(columns = ["Location"], inplace=True)
-
+    L_stations["Lat"] = L_stations["Lat"].astype(float)
+    L_stations["Long"] = L_stations["Long"].astype(float)
 
     return L_stations
 
@@ -105,17 +68,6 @@ def clean_bus_stations(bus_stations_csv):
     df = df.rename(index=float, columns={"POINT_Y": "Lat", "POINT_X": "Long"})
 
     return df
-
-def merge_apartments_data(apartments_dataset, other_set):
-    '''
-    Merge apartments with L stations and/or bustations.
-    m:m merge (every obs in apartments_dataset has to merge with 
-    every obs in the L_stations data)
-
-    Calculate manhatttan dist and drop everything less than .5 miles away from
-    each location
-    '''
-    pass
 
 
 def cartesian_product(df1_ind, df2_ind):
@@ -144,24 +96,7 @@ def cartesian_product(df1_ind, df2_ind):
     return df
 
 
-# def create_cross_join(apartments, l_stations):
-#     cross_product_ind = cartesian_product(apartments.index, l_stations["STOP_ID"])
-
-#     l_stations_fil = l_stations.filter(["STOP_ID", "Lat", "Long"], axis=1)
-    
-#     apartments["apt_ind"] = apartments.index
-#     apartments_fil = apartments.filter(["apt_ind", "Lat", "Long"], axis=1)
-#     merged = cross_product_ind.join(apartments_fil, on="apt_ind", lsuffix= "apt_")
-#     merged = merged.rename(index=float, columns={"Lat": "Lat_apt", "Long": "Long_apt"})
-
-#     merged = merged.merge(l_stations_fil, how="outer", on="STOP_ID")
-#     merged = merged.rename(index=float, columns={"Lat": "Lat_stop", "Long": "Long_stop"})
-#     merged.Lat_stop = merged.Lat_stop.astype(float)
-#     merged.Long_stop = merged.Long_stop.astype(float)
-
-#     return merged
-
-def create_cross_join(df1, df2, additional_columns=None):
+def create_cross_join(df1, df2, suffixes, additional_columns=None):
     '''
     creates a dataframe with the columns: 
         df1 index, df2 index, lat1, long1, lat2, long2
@@ -172,6 +107,8 @@ def create_cross_join(df1, df2, additional_columns=None):
     Inputs: 
         df1: pandas dataframe
         df2: pandas dataframe
+        suffixes: a tuple of the suffixes on the columns in the final dataframe
+                associated with the df1 and df2 respectively.
         additional_columns: list of columns to include in final dataframe
             in addition to the ones listed.
 
@@ -188,10 +125,15 @@ def create_cross_join(df1, df2, additional_columns=None):
     df2_fil['ind2'] = df2.index.astype(str)
     
     merged = cross_product_ind.merge(df1_fil, how="outer", on="ind1")
-    merged = merged.merge(df2_fil, how="outer", on="ind2", suffixes=("_df1", "_df2"))
+    merged = merged.merge(df2_fil, how="outer", on="ind2", suffixes=suffixes)
 
+    #rename index columns to reflect suffixes
+    suffix1, suffix2 = suffixes
+    re_ind1 = "ind" + suffix1
+    re_ind2 = "ind" + suffix2
+    merged = merged.rename(columns={"ind1": re_ind1, "ind2": re_ind2})
     #make all the lat longs floats
-    merged = merged.astype({"Lat_df1": float, "Lat_df2":float, "Long_df1":float, "Long_df2":float})
+    # merged = merged.astype({"Lat_df1": float, "Lat_df2":float, "Long_df1":float, "Long_df2":float})
 
     return merged
 
@@ -228,48 +170,127 @@ def assign_transit_score(df):
     to more distant amenities, with no points given after
     a 30 minute walk.
     '''
-    #only keep bus stops that are within .5 miles of apartments
-    # sub[]
-    # quart = np.where(sub['distance'] <= .25, 1, 0)
-    # sub['wi_quart_mi'] = 0
-    # sub['wi_half_mi'] = 0
-
-    # sub.loc[sub['distance'] <= .5, 'wi_half_mi'] = 1
-    # sub.loc[sub['distance'] <= .25, 'wi_quart_mi'] = 1
-
-    # sub
-    # sub.loc
-
-    # df.loc[df['used'] == 1.0, 'alert'] = 'Full'
     df['wi_1_mi'] = np.where(df['distance'] <= 1, 1, 0)    
     df['wi_3_quart'] = np.where(df['distance'] <= .75, 1, 0)
     df['wi_half_mi'] = np.where(df['distance'] <= .5, 1, 0)
     df['wi_quart_mi'] = np.where(df['distance'] <= .25, 1, 0)
 
-    sub_agg = df.groupby('apt_ind', as_index=False)[['wi_quart_mi', 'wi_half_mi', 'wi_3_quart', 'wi_1_mi']].sum()
+    sub_agg = df.groupby('ind_apt', as_index=False)[['wi_quart_mi', 'wi_half_mi', 'wi_3_quart', 'wi_1_mi']].sum()
 
-    # df['profitable'] = np.where(df['price']>=15.00, True, False)
     return sub_agg
 
+def read_clean_landlords(problem_landlords_filepath):
+    '''
+    Read and clean the problem lands lords csv from the 
+    CHA site.
 
-def go(apartments, l_stations_filepath):
+    Downloaded from: 
+    https://www.chicago.gov/city/en/depts/bldgs/supp_info/building-code-scofflaw-list.html
+    '''
+    landlords = pd.read_csv(problem_landlords_filepath)
+    landlords = landlords.rename(columns={"LONGITUDE": "Long", "LATITUDE": "Lat", "ADDRESS": "Address"})
+    landlords_sub = landlords.filter(["Address", "Long", "Lat"], axis=1)
+
+    return landlords_sub
+
+
+def landlords_apt_fuzzy_match(apt_df, landlords_df, output_filepath):
+    '''
+    Do a fuzzy match by gps coordinates on the dataset with problem
+    landlords and apartments. Match landlords and apartments that are
+    within .1 miles of each other. Export a csv with these matches,
+    and then manually check if the addresses are the same.
+    '''
+    df = create_cross_join(apt_df, landlords_df, ("_apt", "_ll"), \
+                           additional_columns=["Address"])
+    df['distance'] = haversine(df["Long_apt"], df["Lat_apt"], df["Long_ll"], \
+                             df["Lat_ll"])
+    df['v_close'] = np.where(df['distance'] <= .05, 1, 0)
+    df_fil = df[df['v_close']==1]
+
+    df_fil = df_fil.filter(["ind_apt", "ind_ll", "Address_apt", "Address_ll", \
+                            "distance"], axis=1).reset_index(drop=True)
+
+    #output a csv to manually check if these apartments are the same
+    df_fil.to_csv(output_filepath)
+
+    return df_fil
+
+
+def import_and_join_manual_landlord_check(apt_df, manual_check_filepath_csv):
+    '''
+    Imports the csv with apartment addresses and problem landlords
+    within .1 miles of each other (the csv outputted by landlords_apt_fuzzy_match
+    , after a person has manually checked whether the apartment
+    and landlord addresses are the same.
+
+    Input:
+        -apt: full apartment dataframe
+        -manual_check_filepath_csv: filepath of the csv file with a manually
+            checked column - 1 for same_address, NaN if not 
+    '''
+    df = pd.read_csv(manual_check_filepath_csv)
+    df['bad_landlord'] = np.where(df['same_address'] == 1, True, False)
+    sub = df.filter(["ind_apt", "bad_landlord"], axis=1)
+    sub["ind_apt"] = sub["ind_apt"].astype(str)
+    # return sub
+    merged = apt_df.merge(sub, how="outer", on="ind_apt") 
+
+    merged['bad_landlord'] = merged['bad_landlord'].fillna(False)
+
+    return merged
+
+
+def go(apartments, 
+       l_stations_filepath, 
+       output_filepath,
+       problem_landlords_filepath=None, 
+       landlords_apt_fuzzy_match_csv_output=None, 
+       manual_checked_bad_landlord_filepath=None
+       ):
     '''
     Inputs: 
-        - l_stations_filepath: filepath for csv of the locations of the l_stations
         - apartments (pandas dataframe that has been create and cleaned in
                         process_and_merge.py 
+        - l_stations_filepath: filepath for csv of the locations of the l_stations
+        - bad_landlord_filepath: This is the csv containing a list of the 
+            problem landlords identified by the city, with the addresses of the 
+            landlord's buildings, and the lat and long coordinates.
+            Note: In order to match these landlord addresses to apartment
+                addresses, we output a csv with a list of the apartments
+                matched to problem landlord buildings that are within .01 miles
+                from each other. Then we do a manual check to see if in 
+                fact the apartment has the same address and is in the same 
+                building as the problem landlord. If a programmer does not want
+                to do this manual check, then the bad_landlord column will not
+                be included in the final output.
+        - manual_checked_bad_landlord_filepath: This is the csv with that
+            contains the apartments within .05 miles of a bad landlord, AFTER
+            a programmer has manually checked whether the apartments addresses
+            are the same as a bad landlord address. 
+            Note: We have done this, but a programmer does not have to do this
+            step. If they do not, then the bad_landlord column will not be 
+            included in the final ouput.
     '''
     l_stations = clean_L_stations(l_stations_filepath)
-    df = create_cross_join(apartments, l_stations) 
-    df = df.rename(columns={"Lat_df1": "Lat_apt", "Long_df1": "Long_apt", \
-                            "Lat_df2": "Lat_stop", "Long_df2": "Long_stop", \
-                            "ind1": "apt_ind", "ind2": "l_stops_ind"})
-
+    df = create_cross_join(apartments, l_stations, ("_apt", "_stop")) 
     df['distance'] = haversine(
-                    df["Long_apt"], df["Lat_apt"], df["Long_stop"], df["Lat_stop"])
+                    df["Long_apt"], df["Lat_apt"], 
+                    df["Long_stop"], df["Lat_stop"])
+    apt_w_l_stations = assign_transit_score(df)
 
-    grouped = assign_transit_score(df)
+    if problem_landlords_filepath:
+        #exports a csv with a fuzzy match of apartments with bad landlord
+        #buildings
+        landlords_df = read_clean_landlords(problem_landlords_filepath)
+        landlords_apt_fuzzy_match(apartments,
+                                  landlords_df, 
+                                  landlords_apt_fuzzy_match_csv_output)
+        #uses the csv above that has been manually checked and saved as a new file 
+        apt_w_l_stations = import_and_join_manual_landlord_check(
+                                            apt_w_l_stations, 
+                                            manual_checked_bad_landlord_filepath)
 
-    grouped.to_csv("processed_data/transit_info.csv")
+    apt_w_l_stations.to_csv(output_filepath)
 
-    return grouped
+    return apt_w_l_stations
