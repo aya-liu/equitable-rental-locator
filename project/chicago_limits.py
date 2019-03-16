e367016a68cb66f565cd0efac839bca4a8e17219
Bhargavi Ganesh <bhargaviganesh@Bhargavis-MacBook-Air.local>
2019-03-16 00:04:27 -0500

    added chicago limits file

--- project/chicago_limits.py
+++ project/chicago_limits.py
@@ -1,8 +1,9 @@
 import geopandas
 
-shapefile_name = "Boundaries - Census Blocks - 2010.geojson"
+shapefile_name = "Boundaries-CensusBlocks-2010.geojson"
 
 def get_city_geoids(shapefile_name):
 	city_blocks = geopandas.read_file(shapefile_name)
 	city_blocks['GEOID'] = city_blocks['geoid10'].str.slice(start=0, stop=12)
-	return city_blocks['GEOID']
+	city_blocks['GEOID'].to_csv('list_of_chi_geoids.csv')
+
