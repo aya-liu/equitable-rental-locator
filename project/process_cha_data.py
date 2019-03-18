431877b02410ada29d541bd2f839d99c0d5e3210
Bhargavi Ganesh <bganesh@cs.uchicago.edu>
2019-03-18 14:31:08 -0500

    pylinted

--- project/process_cha_data.py
+++ project/process_cha_data.py
@@ -19,11 +19,11 @@ def process_cha_data(cha_dict_raw, blocks_filename, zillow_filename):
     gcha = convert_to_gdf(cha)
     cha_with_geoid = add_blocks_to_cha(gcha, blocks_filename)
     cha_geoid_zillow = add_zillow_regionid_to_cha(cha_with_geoid, 
-                                                    zillow_filename)
+                                                  zillow_filename)
     return cha_geoid_zillow
 
 
-def load_and_clean_cha(CHA_filename):
+def load_and_clean_cha(cha_filename):
     '''
     Load and clean CHA data.
     
@@ -33,18 +33,18 @@ def load_and_clean_cha(CHA_filename):
         (DataFrame) clean CHA data
     '''
     # load raw CHA data
-    with open(CHA_filename, "rb") as f:       
+    with open(cha_filename, "rb") as f:    
         d = pickle.load(f)
     cha = pd.DataFrame.from_dict(data=d, orient="index")
     
     # clean CHA data
-    cols = ['Address','Monthly Rent','Property Type','Bath','Bed',
-            'Availability','Contact','URL','Lat','Long']
+    cols = ['Address', 'Monthly Rent', 'Property Type', 'Bath', 'Bed',
+            'Availability', 'Contact', 'URL', 'Lat', 'Long']
     cha = cha[cols]
     cha.Long = -1 * cha.Long
 
     # correct an one-off location error
-    cha.loc["4545145", "Long"] = -87.66593 
+    cha.loc["4545145", "Long"] = -87.66593
     cha.loc["4545145", "Lat"] = 41.772175
 
     return cha
@@ -73,12 +73,12 @@ def add_blocks_to_cha(gcha, blocks_filename):
     Returns: (GeoDataFrame) CHA geodataframe with block group GEOIDs
     '''
     blocks = geopandas.read_file(blocks_filename).filter(
-                                            ['geometry', 'GEOID'])
+        ['geometry', 'GEOID'])
     blocks.crs = {'init': 'epsg:4326'}
     gcha.crs = {'init': 'epsg:4326'}
 
     cha_with_geoid = geopandas.sjoin(gcha, blocks, how="left", 
-                                                    op='intersects')
+                                     op='intersects')
     cha_with_geoid.drop('index_right', axis=1, inplace=True)
     return cha_with_geoid
 
@@ -97,7 +97,7 @@ def add_zillow_regionid_to_cha(gcha, zillow_filename):
     gcha.crs = {'init': 'epsg:4326'}
 
     cha_geoid_zillow = geopandas.sjoin(gcha, zillow_neighborhoods, 
-                                        how="left", op='intersects')
+                                       how="left", op='intersects')
     # drop unmatches and extra columns
     cha_geoid_zillow.dropna(axis=0, subset=["index_right"], inplace=True)
     cha_geoid_zillow.drop('index_right', axis=1, inplace=True)
@@ -117,22 +117,21 @@ def fill_zillow_na(cha_geoid_zillow):
                                         Region attributes
     Returns: a clean GeoDataFrame
     '''
-    
     # drop rows with false addresses
-    cha_geoid_zillow.drop(index = ['4763759','4796475'], inplace=True)
+    cha_geoid_zillow.drop(index=['4763759', '4796475'], inplace=True)
     # fill invariant NA values
     cha_geoid_zillow.fillna({"State": "IL", "County": "Cook", 
-        "City": "Chicago"}, inplace=True)
+                             "City": "Chicago"}, inplace=True)
     # manually look up Zillow Regions on zillow.com  
     unmatches = cha_geoid_zillow[cha_geoid_zillow["index_right"].isna()]
     values = {'4567448': ['Gresham', '269571'],
-         '4590237': ['Gresham', '269571'],
-         '4618006': ['Cabrini Green', '403302'],
-         '4632177': ['Park Manor', '403356'],
-         '4640435': ['Marquette Park', '403148'],
-         '4646604': ['Rogers Park', '269605'],
-         '4729058': ['Morgan Park', '269595'],
-         '4729219': ['West Town', '269615']}
+			  '4590237': ['Gresham', '269571'],
+			  '4618006': ['Cabrini Green', '403302'],
+			  '4632177': ['Park Manor', '403356'],
+			  '4640435': ['Marquette Park', '403148'],
+			  '4646604': ['Rogers Park', '269605'],
+			  '4729058': ['Morgan Park', '269595'],
+			  '4729219': ['West Town', '269615']}
     for i in unmatches.index:
         cha_geoid_zillow.loc[i, ["Name", "RegionID"]] = values[i]
 
