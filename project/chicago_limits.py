e942755340273b5691086175502c5f4f220f7234
Aya Liu <lzy@Aya-Lius-MacBook.local>
2019-03-18 15:30:15 -0500

    finalized

--- project/chicago_limits.py
+++
@@ -1,9 +0,0 @@
-import geopandas
-
-shapefile_name = "Boundaries-CensusBlocks-2010.geojson"
-
-def get_city_geoids(shapefile_name):
-	city_blocks = geopandas.read_file(shapefile_name)
-	city_blocks['GEOID'] = city_blocks['geoid10'].str.slice(start=0, stop=12)
-	city_blocks['GEOID'].to_csv('list_of_chi_geoids.csv')
-
