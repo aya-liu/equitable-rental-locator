16eb543a75a4d268522604c0aa6ee9a715d5d5f2
Bhargavi Ganesh <bhargaviganesh@Bhargavis-MacBook-Air.local>
2019-03-18 15:13:12 -0500

    committed everything

--- project/aggregate_tables_v1.py
+++ project/aggregate_tables_v1.py
@@ -1,23 +1,99 @@
 import pandas as pd
 import numpy as np
+import geopandas
 
 
-#delete when done
 def read_locator_db(filepath):
-    ld = pd.read_csv(filepath, index_col="index")
+    col_types = {
+    'index': str,
+    'Address': str,
+    'Monthly Rent': 'int64',
+    'Propterty Type': str,
+    'Bath': float,
+    'Bed': float,
+    'Availability': str,
+    'Contact': str,
+    'URL': str,
+    'Lat': float,
+    'Long': float,
+    'Coordinates': str,
+    'GEOID': str,
+    'State': str,
+    'County': str,
+    'City': str,
+    'Neighborhood': str,
+    'RegionID': str,
+    'parent-location': str,
+    'population': float,
+    'renter-occupied-households': float,
+    'median-gross-rent': float,
+    'median-household-income': float,
+    'median-property-value': float,
+    'pct-white': float,
+    'pct-af-am': float,
+    'pct-hispanic': float,
+    'pct-asian': float,
+    'eviction-filings': float,
+    'evictions': float,
+    '2016_evict_rate': float,
+    '2016_evict_filing_rate': float,
+    '2011-2015_rent_perc_change': float,
+    '2015-2019_rent_perc_change': float,
+    'potential_bad_landlord': bool,
+    'bad_landlord_address': str,
+    'num_stops_quart_mi': int,
+    'num_stops_half_mi': int,
+    'num_stops_3quart_mi': int,
+    'num_stops_1_mi': int,
+    'er_percentile': float,
+    'efr_percentile': float,
+    'stop_wi_quart_mi': int,
+    'stop_wi_half_mi': int,
+    'pot_bad_landlord': int
+    }
+
+    ld = pd.read_csv(filepath, index_col="index", dtype=col_types)
 
     return ld
 
 
 def read_block_group_data(block_groups_csv):
-    evictions = pd.read_csv(block_groups_csv)
-    evictions_2016 = evictions[evictions['year']==2016]
+    col_types = {
+    'GEOID': str,
+    'year': str,
+    'name': str,
+    'parent-location': str,
+    'population': float,
+    'poverty-rate': float,
+    'renter-occupied-households': float,
+    'pct-renter-occupied': float,
+    'median-gross-rent': float,
+    'median-household-income': float,
+    'median-property-value': float,
+    'rent-burden': float,
+    'pct-white': float,
+    'pct-af-am': float,
+    'pct-hispanic': float,
+    'pct-am-ind': float,
+    'pct-asian': float,
+    'pct-nh-pi': float,
+    'pct-multiple': float,
+    'pct-other': float,
+    'eviction-filings': float,
+    'evictions': float,
+    'eviction-rate': float,
+    'eviction-filing-rate': float,
+    'low-flag': int,
+    'imputed': int,
+    'subbed': int
+    }
 
+    evictions = pd.read_csv(block_groups_csv, dtype=col_types)
+    evictions_2016 = evictions[evictions['year']=='2016']
     race_vars = ['pct-white', 'pct-af-am', 'pct-hispanic', 'pct-asian']
     for var in race_vars:
         new_var = var[4:] + '_pop'
         evictions_2016[new_var] = (evictions_2016[var]/100)*evictions_2016['population']
-
 
     evictions_2016['people_in_poverty'] = evictions_2016['population']* \
                             (evictions_2016['poverty-rate']/100)
@@ -60,8 +136,20 @@ def aggregate_cha_by_geoid(locator_database):
 
     return ld_by_geoid
 
-def make_master_agg_dataset(list_chicago_geoids_csv, block_group_df, 
-                                geoid_to_region_id_map_csv, ld_by_geoid):
+def create_city_blocks(city_blocks_csv, zillow_shapefile):
+  # filename = "data/Boundaries-CensusBlocks-2010.geojson"
+  # filename = "data/ZillowNeighborhoods-IL.shp"
+  city_blocks_df = geopandas.read_file(city_blocks_csv)
+  city_blocks_df['GEOID'] = city_blocks_df['geoid10'].str.slice(start=0, stop=12)
+  city_blocks_df.crs = {'init' :'epsg:4326'}
+  zillow_neighborhoods = geopandas.read_file(zillow_shapefile)
+  zillow_neighborhoods.crs = {'init' :'epsg:4326'}
+  block_zillow_merge = geopandas.sjoin(city_blocks_df, zillow_neighborhoods, how="left", op="intersects")
+
+  return block_zillow_merge
+
+
+def make_master_agg_dataset(blocks_zillow_merge, block_group_df, ld_by_geoid):
     '''
     1.  read in list of geoids in chicago
     2. read in list of geoids mapped to regions ids and neighborhoods
@@ -72,20 +160,19 @@ def make_master_agg_dataset(list_chicago_geoids_csv, block_group_df,
     chicago geoids: "list_of_chi_geoids.csv"
     geoid_to_region_id_map_csv: "all_geography_merge.csv"
     '''
-    chi_geoid = pd.read_csv(list_chicago_geoids_csv, 
-                            header=None, names=["index", "GEOID"], usecols=[1])
-    chi_geoid = chi_geoid.drop_duplicates('GEOID', keep='first')
+    # chi_geoid = blocks_zillow_merge['GEOID']
+    # # chi_geoid = pd.read_csv(list_chicago_geoids_csv, 
+    # #                         header=None, names=["index", "GEOID"], usecols=[1])
+    # chi_geoid = chi_geoid.drop_duplicates()
+    df_to_merge = blocks_zillow_merge[['GEOID', 'Name', 'RegionID']]
+    df_to_merge = df_to_merge.drop_duplicates('GEOID', keep="first")
+    # geoid_neigh = pd.read_csv(geoid_to_region_id_map_csv, usecols=[1,2,3])
+    # geoid_neigh = geoid_neigh.drop_duplicates('GEOID', keep='first')
 
-    geoid_neigh = pd.read_csv(geoid_to_region_id_map_csv, usecols=[1,2,3])
-    geoid_neigh = geoid_neigh.drop_duplicates('GEOID', keep='first')
-
-    chi_geoid_neigh = chi_geoid.merge(geoid_neigh, how="left", on="GEOID")
-    evictions_full = chi_geoid_neigh.merge(block_group_df, 
-                                           how="left", on="GEOID")
+    # chi_geoid_neigh = chi_geoid.merge(geoid_neigh, how="left", on="GEOID")
+    evictions_full = pd.merge(df_to_merge, block_group_df, how="left", on="GEOID")
     evictions_full = evictions_full.rename(columns={'Name': 'Neighborhood'})
-
-    agg_df_by_geoid = evictions_full.merge(ld_by_geoid, 
-                                        how="left", on="GEOID", indicator=True)
+    agg_df_by_geoid = evictions_full.merge(ld_by_geoid, how="left", on="GEOID", indicator=True)
 
     return agg_df_by_geoid
 
@@ -143,7 +230,7 @@ def master_agg_by_neighborhood(agg_df_by_geoid, output_file):
 def agg_CHA_non_CHA_compare(master_agg_by_neigh, output_file):
     '''
     Compare neighborhoods with at least 10 homes available in them to homes
-    with less than 10. 96% of the HCV homes in the list are in neighborhoods 
+    with less than 10.96% of the HCV homes in the list are in neighborhoods 
     with at least 10 homes available.
 
     https://interactive.wbez.org/curiouscity/segregation-map/ 
@@ -174,7 +261,6 @@ def agg_CHA_non_CHA_compare(master_agg_by_neigh, output_file):
     
     agg = agg.rename(columns={'num_cha_properties' : 'num_neighborhoods'})
 
-
     neigh_summary = agg[['num_neighborhoods','poverty_rate', 'less_20_perc_pov', 
                         'eviction_rate', 'integrated',  'maj_black', 'maj_white',
                          'maj_latino']]
@@ -201,20 +287,24 @@ def num_homes_in_mobility_neigh(master_agg_by_neigh, output_file):
 
     return neigh_by_pov
 
+
+# def generate_map():
+#   cha_geoid_zillow = 
+#   count_series = cha_geoid_zillow.groupby('GEOID').size()
+
 #reorder
 #perhaps make them into percentages? 
 #make pretty in excel.
 def build_agg_tables(locator_database_csv, block_groups_csv, 
-       list_chicago_geoids_csv, geoid_to_region_id_map_csv,
-       output_file1, output_file2, output_file3):
+  city_blocks_csv, zillow_shapefile, output_file1, output_file2, output_file3):
 
-    locator_database = pd.read_csv(locator_database_csv, index_col="index")
+    locator_database = read_locator_db(locator_database_csv)
     block_group_df = read_block_group_data(block_groups_csv)
     ld_by_geoid = aggregate_cha_by_geoid(locator_database)
-    agg_df_by_geoid = make_master_agg_dataset(list_chicago_geoids_csv, 
-                                             block_group_df, 
-                                             geoid_to_region_id_map_csv, 
-                                             ld_by_geoid)
+    blocks_zillow_merge = create_city_blocks(city_blocks_csv, zillow_shapefile)
+    agg_df_by_geoid = make_master_agg_dataset(blocks_zillow_merge,
+                                              block_group_df, 
+                                              ld_by_geoid)
 
     master_agg_by_neigh = master_agg_by_neighborhood(agg_df_by_geoid, output_file1)
     agg_CHA_non_CHA = agg_CHA_non_CHA_compare(master_agg_by_neigh, output_file2)
@@ -223,11 +313,13 @@ def build_agg_tables(locator_database_csv, block_groups_csv,
 
 LOCATOR_DB_CSV = "processed_data/locator_database.csv"
 BLOCK_GROUPS_CSV = "data/block-groups.csv" 
-CHICAGO_GEOIDS = "list_of_chi_geoids.csv"
-GEOID_TO_REGIONID = "all_geography_merge.csv"
-OUTPUT_FILE1 = "processed_data/agg_table_by_neighborhood.csv"
-OUTPUT_FILE2 = "processed_data/agg_table_hsv_non_hsv.csv"
-OUTPUT_FILE3 = "processed_data/homes_in_pov_neighborhood.csv"
+CITY_BLOCKS_CSV = "data/Boundaries-CensusBlocks-2010.geojson"
+ZILLOW_SHAPEFILE = "data/ZillowNeighborhoods-IL.shp"
+# CHICAGO_GEOIDS = "list_of_chi_geoids.csv"
+# GEOID_TO_REGIONID = "all_geography_merge.csv"
+OUTPUT_FILE1 = "processed_data/agg_table_by_neighborhood_bg.csv"
+OUTPUT_FILE2 = "processed_data/agg_table_hsv_non_hsv_bg.csv"
+OUTPUT_FILE3 = "processed_data/homes_in_pov_neighborhood_bg.csv"
 
 
 def main():
@@ -238,8 +330,8 @@ def main():
     db = build_agg_tables(
         LOCATOR_DB_CSV, 
         BLOCK_GROUPS_CSV, 
-        CHICAGO_GEOIDS, 
-        GEOID_TO_REGIONID,
+        CITY_BLOCKS_CSV,
+        ZILLOW_SHAPEFILE, 
         OUTPUT_FILE1,
         OUTPUT_FILE2,
         OUTPUT_FILE3)
